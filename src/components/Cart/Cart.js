7f61c1b6c2cd925654443dd92e25c112ea1c0e41
Christian Ogniew <christianogniew@gmail.com>
2023-08-05 09:17:17 -0300

    completo el carrito

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,48 +1,3 @@
-// import React, { useContext } from "react";
-// import "./Cart.css";
-// import { CartContext } from "../../context/CartContext";
-// import CartItem from "../CartItem/CartItem";
-// import { Link } from "react-router-dom";
-
-// const Cart = () => {
-//   const { cart, clearCart, totalQuantity } = useContext(CartContext);
-
-//   const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
-
-//   if (totalQuantity === 0) {
-//     return (
-//       <div>
-//         <h1> No hay items en el carrito </h1>
-//         <Link to="/" className="Option">
-//           Productos
-//         </Link>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div className="CartContainer">
-//       <div className="CartItemsContainer">
-//         {cart.map((p) => (
-//           <CartItem key={p.id} item={p} />
-//         ))}
-//       </div>
-
-//       <div className="CartSummary">
-//         <h3>Total: ${total}</h3>
-//         <button onClick={() => clearCart()} className="Button">
-//           Limpiar Carrito
-//         </button>
-//         <Link to="/checkout" className="Option">
-//           Checkout
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Cart;
-////////////////// con form /////////////////
 import React, { useContext, useState } from "react";
 import "./Cart.css";
 import { CartContext } from "../../context/CartContext";
@@ -57,6 +12,7 @@ const Cart = () => {
     mail: "",
   });
 
+  const [submitted, setSubmitted] = useState(false);
   const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
   const handleInputChange = (event) => {
@@ -66,9 +22,15 @@ const Cart = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // aca podemos hacer lo que querramos con la info de los datos
 
-    console.log(formData);
+    const buyerInfo = { ...formData };
+    const order = {
+      buyer: buyerInfo,
+      items: cart,
+      total: total,
+    };
+    console.log(order);
+    setSubmitted(true);
   };
 
   if (totalQuantity === 0) {
@@ -92,52 +54,54 @@ const Cart = () => {
       <button onClick={() => clearCart()} className="btn-limpiar">
         Limpiar Carrito
       </button>
-      <div className="CartForm">
-        <h3>Completa tus datos</h3>
-        <form onSubmit={handleSubmit}>
-          <div className="FormInput">
-            <label htmlFor="nombre">Nombre</label>
-            <input
-              type="text"
-              id="nombre"
-              name="nombre"
-              value={formData.nombre}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <div className="FormInput">
-            <label htmlFor="apellido">Apellido</label>
-            <input
-              type="text"
-              id="apellido"
-              name="apellido"
-              value={formData.apellido}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <div className="FormInput">
-            <label htmlFor="mail">E-Mail</label>
-            <input
-              type="email"
-              id="mail"
-              name="mail"
-              value={formData.mail}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-        </form>
-      </div>
 
       <div className="CartSummary">
         <h3>Total: ${total}</h3>
-
-        <Link to="/checkout" className="Option">
-          Checkout
-        </Link>
       </div>
+
+      {!submitted && (
+        <div className="CartForm">
+          <h3>Completa tus datos</h3>
+          <form onSubmit={handleSubmit}>
+            <div className="FormInput">
+              <label htmlFor="nombre">Nombre</label>
+              <input
+                type="text"
+                id="nombre"
+                name="nombre"
+                value={formData.nombre}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className="FormInput">
+              <label htmlFor="apellido">Apellido</label>
+              <input
+                type="text"
+                id="apellido"
+                name="apellido"
+                value={formData.apellido}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className="FormInput">
+              <label htmlFor="mail">E-Mail</label>
+              <input
+                type="email"
+                id="mail"
+                name="mail"
+                value={formData.mail}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Checkout
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
